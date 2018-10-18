2e0e6720f0b0e999aece61d06a28c589bd1fd4a2
Oscar <oredondo@seyte.com>
2018-10-18 12:35:45 +0200

    New type attachments: XML

--- ImapClient/TypeAttachments.php
+++ ImapClient/TypeAttachments.php
@@ -19,7 +19,7 @@ class TypeAttachments
      *
      * @var array
      */
-    private static $types = array('JPEG', 'PNG', 'GIF', 'PDF', 'X-MPEG', 'MSWORD', 'OCTET-STREAM', 'TXT', 'TEXT', 'MWORD', 'ZIP', 'MPEG', 'DBASE', 'ACROBAT', 'POWERPOINT', 'BMP', 'BITMAP');
+    private static $types = array('JPEG', 'PNG', 'GIF', 'PDF', 'X-MPEG', 'MSWORD', 'OCTET-STREAM', 'TXT', 'TEXT', 'MWORD', 'ZIP', 'MPEG', 'DBASE', 'ACROBAT', 'POWERPOINT', 'BMP', 'BITMAP', 'XML');
 
     /**
      * Get the allowed types.
